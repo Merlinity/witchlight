229514d2519ff520881cd35d0247c516e8b528c4
Merlin Aubel <merlin.aubel@active-logistics.com>
2023-04-06 06:16:28 +0200

    refactorings

--- src/app/ListHeroes.tsx
+++ src/app/ListHeroes.tsx
@@ -5,11 +5,11 @@ import {PlayerCharacter} from "../classes/player-character";
 export const ListHeroes = () => {
     const heroes: PlayerCharacter[] = HeroService();
     return (
-        <div className="HeroList">{heroes.map(hero =>
-            (<div className="Hero">
-                <span><b>{hero.name}</b>,</span>
-                <span>{hero.race},</span>
-                <span>{
+        <div className="hero-list">{heroes.map(hero =>
+            (<div key={'hero-' + hero.name} className="hero">
+                <span className="hero-name"><b>{hero.name}</b>,</span>
+                <span className="hero-race">{hero.race},</span>
+                <span className="hero-classes">{
                     hero.class?.map(
                         cl => cl.name + " (" + cl.level + ')'
                     ).join("/")
